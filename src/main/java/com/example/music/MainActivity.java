bea5c7c2ed777860eff7ddcf7a1ec69f3226ba37
cyc05326 <cyc05326@nate.com>
2021-05-30 13:59:32 +0900

    BGM-test2

--- src/main/java/com/example/music/MainActivity.java
+++ src/main/java/com/example/music/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.music;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mediaPlayer;
     Button btn_play, btn_stop;
+    Button btn_play2,btn_stop2;
+    Button btn_play3,btn_stop3;
+    Button btn_play4,btn_stop4;
+    Button btn_play5,btn_stop5;
+
+
+    //종료 될때
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer!= null){
+            mediaPlayer.release();
+            mediaPlayer=null;
+
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +36,61 @@ public class MainActivity extends AppCompatActivity {
 
         btn_play =(Button)findViewById(R.id.btn_play);
         btn_stop = (Button)findViewById(R.id.btn_stop);
+
+        btn_play2 =(Button)findViewById(R.id.btn_play2);
+        btn_stop2 = (Button)findViewById(R.id.btn_stop2);
+
+        btn_play3 =(Button)findViewById(R.id.btn_play3);
+        btn_stop3 = (Button)findViewById(R.id.btn_stop3);
+
+        btn_play4 =(Button)findViewById(R.id.btn_play4);
+        btn_stop4 = (Button)findViewById(R.id.btn_stop4);
+
+        btn_play5 =(Button)findViewById(R.id.btn_play5);
+        btn_stop5 = (Button)findViewById(R.id.btn_stop5);
         //시작버튼
+
         btn_play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startService(new Intent(getApplicationContext(), MusicService.class));
+                mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.ncm1);
+                mediaPlayer.start();
+            }
+        });
+
+        btn_play2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.ncm2);
+                mediaPlayer.start();
+                //startService(new Intent(getApplicationContext(), MusicService.class));
+            }
+        });
+
+        btn_play3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.ncm3);
+                mediaPlayer.start();
+                // startService(new Intent(getApplicationContext(), MusicService.class));
+            }
+        });
+
+        btn_play4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.ncm4);
+                mediaPlayer.start();
+                // startService(new Intent(getApplicationContext(), MusicService.class));
+            }
+        });
+
+        btn_play5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.ncm5);
+                mediaPlayer.start();
+                // startService(new Intent(getApplicationContext(), MusicService.class));
             }
         });
 
@@ -30,8 +98,50 @@ public class MainActivity extends AppCompatActivity {
         btn_stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                stopService(new Intent(getApplicationContext(), MusicService.class));
+
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+                // stopService(new Intent(getApplicationContext(), MusicService.class));
             }
         });
+
+        btn_stop2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+                //  stopService(new Intent(getApplicationContext(), MusicService.class));
+            }
+        });
+
+        btn_stop3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+                // stopService(new Intent(getApplicationContext(), MusicService.class));
+            }
+        });
+
+        btn_stop4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+                // stopService(new Intent(getApplicationContext(), MusicService.class));
+            }
+        });
+
+        btn_stop5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+                //stopService(new Intent(getApplicationContext(), MusicService.class));
+            }
+        });
+
+
+
     }
 }
